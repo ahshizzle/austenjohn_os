ffdafe99e0b1c11c0aaea389da790441ab1d1643
escobalj <escobalj@sou.edu>
2016-04-14 10:46:47 -0700

    Header Project

---
+++ Lab1/Header/src/header/Primes.java
@@ -0,0 +1,48 @@
+package header;
+
+//Class to simulate a run time program.
+//Generates list of the first n prime numbers.
+import java.io.*;
+
+public class Primes extends Program
+{
+  private static final long serialVersionUID = 1L;
+
+  public Primes()
+  {
+	super("Prime Numbers Generator");
+  }
+
+  public int run(PrintWriter out, String[] args)
+  {
+	long maxNumber = Long.parseLong(args[1]);
+	if (maxNumber < 1)
+	    return (ILLEGAL_PARAMETER);
+
+	out.println(args[0] + ": List of the first " + maxNumber + " primes");
+	if (maxNumber >= 1)
+	    out.println(2);
+
+	boolean isPrime = true;
+	long currentNumber = 3;
+	for (int i = 2; i <= maxNumber;)
+	{
+	    for (int j = 2; j <= Math.sqrt(currentNumber); j++)
+	    {
+		if (currentNumber % j == 0)
+		{
+		    isPrime = false;
+		    break;
+		}
+	    }
+	    if (isPrime != false)
+	    {
+		out.println(currentNumber);
+		i++;
+	    }
+	    isPrime = true;
+	    currentNumber++;
+	}
+	return SUCCESS;
+  }
+}
---
+++ Lab1/Header/src/header/Tower.java
@@ -0,0 +1,41 @@
+package header;
+
+//Class to simulate a run time program.
+//Solves the Tower of Hanoi problem for n disks.
+import java.io.*;
+
+public class Tower extends Program
+{
+  private static final long serialVersionUID = 1L;
+
+  public Tower()
+  {
+	super("Tower of Hanoi Solver");
+  }
+
+  public int run(PrintWriter out, String[] args)
+  {
+	long maxNumber = Long.parseLong(args[1]);
+	if (maxNumber < 1)
+	    return (ILLEGAL_PARAMETER);
+
+	out.println(args[0] + ": " + maxNumber + " rings");
+
+	solve(out, maxNumber, "1", "2", "3");
+	return SUCCESS;
+  }
+
+  private void solve(PrintWriter out, long n, String start, String auxiliary, String end)
+  {
+	if (n == 1)
+	{
+	    out.println("Move from " + start + " to " + end);
+	}
+	else
+	{
+	    solve(out, n - 1, start, end, auxiliary);
+	    out.println("Move from " + start + " to " + end);
+	    solve(out, n - 1, auxiliary, start, end);
+	}
+  }
+}
